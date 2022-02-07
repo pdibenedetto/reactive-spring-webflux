79cc7a2c5ce92fe0b96a3aa9154278c591a60422
Paul DiBenedetto <pdibenedetto@users.noreply.github.com>
2022-02-06 20:11:20 -0500

    Muscle memory

--- reactive-programming-using-reactor/src/main/java/com/learnreactiveprogramming/service/FluxAndMonoGeneratorService.java
+++ reactive-programming-using-reactor/src/main/java/com/learnreactiveprogramming/service/FluxAndMonoGeneratorService.java
@@ -1,4 +1,38 @@
 package com.learnreactiveprogramming.service;
 
+import lombok.extern.slf4j.Slf4j;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+
+import java.util.List;
+
+@Slf4j
 public class FluxAndMonoGeneratorService {
+
+    public static void main(String[] args) {
+        FluxAndMonoGeneratorService svc = new FluxAndMonoGeneratorService();
+
+        svc.namesFlux().subscribe(log::info);
+
+        svc.nameMono()
+                .subscribe(name -> log.info("Mono.just() " + name));
+    }
+
+    // namesFlux is a publisher
+    public Flux<String> namesFlux() {
+        return Flux.fromIterable(List.of(
+                "alex",
+                "mike",
+                "tiffany",
+                "shannon",
+                "jackie"))
+                .log();
+    }
+
+    // namesMono is a publisher
+    public Mono<String> nameMono() {
+        return Mono.just("Sophia").log();
+    }
+
+
 }
--- reactive-programming-using-reactor/src/test/java/com/learnreactiveprogramming/service/FluxAndMonoGeneratorServiceTest.java
+++ reactive-programming-using-reactor/src/test/java/com/learnreactiveprogramming/service/FluxAndMonoGeneratorServiceTest.java
@@ -1,7 +1,70 @@
 package com.learnreactiveprogramming.service;
 
-import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import reactor.test.StepVerifier;
 
 class FluxAndMonoGeneratorServiceTest {
+    FluxAndMonoGeneratorService svc;
 
+    @BeforeEach
+    void setUp() {
+        svc = new FluxAndMonoGeneratorService();
+    }
+
+    @Test
+    void namesFlux() {
+        // given
+
+        // when
+        var namesFlux = svc.namesFlux();
+
+        // then
+        StepVerifier
+                .create(namesFlux)
+                .expectNext(
+                        "alex",
+                        "mike",
+                        "tiffany",
+                        "shannon",
+                        "jackie")
+                .verifyComplete();
+    }
+
+    @Test
+    void namesFluxCount() {
+        // given
+
+        // when
+        var namesFlux
+                = svc.namesFlux();
+
+        // then
+        StepVerifier
+                .create(namesFlux)
+                .expectNextCount(5)
+                .verifyComplete();
+
+        // then
+        StepVerifier
+                .create(namesFlux)
+                .expectNext("alex")
+                .expectNextCount(4)
+                .verifyComplete();
+    }
+
+    @Test
+    void nameMono() {
+        // given
+
+        // when
+        var nameMono
+                = svc.nameMono();
+
+        // then
+        StepVerifier
+                .create(nameMono)
+                .expectNextCount(1)
+                .verifyComplete();
+    }
 }
